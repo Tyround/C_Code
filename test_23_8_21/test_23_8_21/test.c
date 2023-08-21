6b09c6e7f7a57073cc3f72eaaa636846ff454768
Tyround <142284753+Tyround@users.noreply.github.com>
2023-08-21 19:43:09 +0800

    Add files via upload

---
+++ test_23_8_21/test_23_8_21/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+//复合操作符
+//+=……
+// a = a + 1;
+//a += 1;
+
+//!逻辑反操作
+//将非0变为0，0变为1
+int main()
+{
+
+	int flag = 0;
+	if (flag)
+	{
+
+	}
+
+	if (!flag)
+	{
+	
+	}
+	return 0;
+}
